e68ad7db9d41b709ba4ee79bea5f6a79c228d03c
Dylan Smith <dylantsmith@gmail.com>
2015-03-13 11:36:55 -0500

    Remove "dashboard" view
    The dashboard view is a little confusing to those who don’t need it,
    and isn’t necessary in a very simple example.  So remove it and keep
    just the “home” view.

--- public/modules/home/config/home.client.routes.js
+++ public/modules/home/config/home.client.routes.js
@@ -5,13 +5,8 @@ angular.module("home").config(["$stateProvider",
 
         $stateProvider.
         state("home", {
-            parent: "app",
-            abstract: true,
-            templateUrl: "/assets/modules/home/views/home.client.view.html"
-        }).
-        state("home.dashboard", {
             url: ApplicationConfiguration.rootUrlPrefix,
-            templateUrl: "/assets/modules/home/views/dashboard.client.view.html"
+            templateUrl: "/assets/modules/home/views/home.client.view.html"
         });
     }
 ]);
--- public/modules/home/controllers/dashboard.client.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-angular.module("home").controller("DashboardCtrl", ["$scope",
-    function($scope) {
-
-    }
-]);
